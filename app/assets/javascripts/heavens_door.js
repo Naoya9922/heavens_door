60d70942474455e176ef2b2cfd06451fda1d6ab1
Naoya Ueda <uedanaoya1174@gmail.com>
2019-04-01 15:25:05 +0900

    localhostとhttps以外はnavigatorが使えないのでそのままalertに出す

--- app/assets/javascripts/heavens_door.js
+++ app/assets/javascripts/heavens_door.js
@@ -1,27 +1,34 @@
 (fn => {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+  if (
+    document.attachEvent
+      ? document.readyState === "complete"
+      : document.readyState !== "loading"
+  ) {
     fn();
   } else {
-    if ((typeof(Turbolinks) !== 'undefined') && (Turbolinks)) {
-      if (Turbolinks.EVENTS && Turbolinks.EVENTS.LOAD) {  // turbolinks-classic
+    if (typeof Turbolinks !== "undefined" && Turbolinks) {
+      if (Turbolinks.EVENTS && Turbolinks.EVENTS.LOAD) {
+        // turbolinks-classic
         document.addEventListener(Turbolinks.EVENTS.LOAD, fn);
-        document.addEventListener('DOMContentLoaded', fn);
-      } else {  // turbolinks
-        document.addEventListener('turbolinks:load', fn);
+        document.addEventListener("DOMContentLoaded", fn);
+      } else {
+        // turbolinks
+        document.addEventListener("turbolinks:load", fn);
       }
-    } else {  // no turbolinks
-      document.addEventListener('DOMContentLoaded', fn);
+    } else {
+      // no turbolinks
+      document.addEventListener("DOMContentLoaded", fn);
     }
   }
 })(() => {
-  const openButton = document.getElementById('heavens-door-open');
-  const closeButton = document.getElementById('heavens-door-close');
-  const copyButton = document.getElementById('heavens-door-copy');
+  const openButton = document.getElementById("heavens-door-open");
+  const closeButton = document.getElementById("heavens-door-close");
+  const copyButton = document.getElementById("heavens-door-copy");
 
-  openButton.addEventListener('click', () => {
-    openButton.style.display = 'none';
-    closeButton.style.display = 'inline';
-    copyButton.style.display = 'inline';
+  openButton.addEventListener("click", () => {
+    openButton.style.display = "none";
+    closeButton.style.display = "inline";
+    copyButton.style.display = "inline";
 
     if (!sessionStorage.heavensDoor) {
       sessionStorage.heavensDoor = `  scenario 'GENERATED' do
@@ -29,19 +36,16 @@
     }
   });
 
-  closeButton.addEventListener('click', () => {
-    openButton.style.display = 'inline';
-    closeButton.style.display = 'none';
-    copyButton.style.display = 'none';
-    sessionStorage.removeItem('heavensDoor');
+  closeButton.addEventListener("click", () => {
+    openButton.style.display = "inline";
+    closeButton.style.display = "none";
+    copyButton.style.display = "none";
+    sessionStorage.removeItem("heavensDoor");
   });
 
-  copyButton.addEventListener('click', () => {
-    navigator.clipboard.writeText(sessionStorage.heavensDoor)
-      .catch(err => {
-        console.error('Could not copy text: ', err);
-        alert(sessionStorage.heavensDoor);
-      });
+  copyButton.addEventListener("click", () => {
+    alert(sessionStorage.heavensDoor);
+    console.log(sessionStorage.heavensDoor);
   });
 
   if (sessionStorage.heavensDoor) {
@@ -55,41 +59,64 @@
     }
   }
 
-  Array.from(document.getElementsByTagName('form')).forEach(form => {
-    form.addEventListener('submit', () => {
+  Array.from(document.getElementsByTagName("form")).forEach(form => {
+    form.addEventListener("submit", () => {
       if (sessionStorage.heavensDoor) {
-        Array.from(form.querySelectorAll('input,textarea,select')).forEach(el => {
-          const target = labelIdForElement(el) || el.id;
+        Array.from(form.querySelectorAll("input,textarea,select")).forEach(
+          el => {
+            const target = labelIdForElement(el) || el.id;
 
-          if (['text', 'textarea', 'search', 'number', 'email', 'url', 'password', 'tel'].includes(el.type)) {
-            if (el.value) {
-              sessionStorage.heavensDoor += `    fill_in '${target}', with: '${el.value}'\n`;
+            if (
+              [
+                "text",
+                "textarea",
+                "search",
+                "number",
+                "email",
+                "url",
+                "password",
+                "tel"
+              ].includes(el.type)
+            ) {
+              if (el.value) {
+                sessionStorage.heavensDoor += `    fill_in '${target}', with: '${
+                  el.value
+                }'\n`;
+              }
+            } else if (el.type == "date") {
+              sessionStorage.heavensDoor += `    fill_in '${target}', with: Date.parse('${
+                el.value
+              }')\n`;
+            } else if (el.type == "select-one") {
+              sessionStorage.heavensDoor += `    select '${
+                el[el.selectedIndex].value
+              }', from: '${target}'\n`;
+            } else if (el.type == "select-multiple") {
+              Array.from(el.selectedOptions).forEach(o => {
+                sessionStorage.heavensDoor += `    select '${
+                  o.value
+                }', from: '${target}'\n`;
+              });
+            } else if (el.type == "radio" && el.checked) {
+              sessionStorage.heavensDoor += `    choose '${el.value}'\n`;
+            } else if (el.type == "checkbox" && el.checked) {
+              sessionStorage.heavensDoor += `    check '${el.value}'\n`;
             }
-          } else if (el.type == 'date') {
-            sessionStorage.heavensDoor += `    fill_in '${target}', with: Date.parse('${el.value}')\n`;
-          } else if (el.type == 'select-one') {
-            sessionStorage.heavensDoor += `    select '${el[el.selectedIndex].value}', from: '${target}'\n`;
-          } else if (el.type == 'select-multiple') {
-            Array.from(el.selectedOptions).forEach(o => {
-              sessionStorage.heavensDoor += `    select '${o.value}', from: '${target}'\n`;
-            })
-          } else if ((el.type == 'radio') && el.checked) {
-            sessionStorage.heavensDoor += `    choose '${el.value}'\n`;
-          } else if ((el.type == 'checkbox') && el.checked) {
-            sessionStorage.heavensDoor += `    check '${el.value}'\n`;
           }
-        })
+        );
 
-        sessionStorage.heavensDoor += `    click_button '${form.querySelector('input[type=submit]').value}'\n\n`;
+        sessionStorage.heavensDoor += `    click_button '${
+          form.querySelector("input[type=submit]").value
+        }'\n\n`;
       }
     });
-  })
+  });
 
-  Array.from(document.getElementsByTagName('a')).forEach(a => {
-    a.addEventListener('click', () => {
+  Array.from(document.getElementsByTagName("a")).forEach(a => {
+    a.addEventListener("click", () => {
       if (sessionStorage.heavensDoor) {
         sessionStorage.heavensDoor += `    click_link '${a.text}'\n\n`;
       }
     });
-  })
-})
+  });
+});
